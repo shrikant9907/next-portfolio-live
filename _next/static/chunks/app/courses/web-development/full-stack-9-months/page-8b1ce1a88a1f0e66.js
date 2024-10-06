5eadf654672ed66eb51da8625bf29fbf2bd6e435
Shrikant Yadav <shrikant9907@gmail.com>
2024-10-06 21:41:27 +0530

    build deploy

---
+++ _next/static/chunks/app/courses/web-development/full-stack-9-months/page-8b1ce1a88a1f0e66.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[38],{},function(n){n.O(0,[164,839,782,812,926,182,170,842,744],function(){return n(n.s=31176)}),_N_E=n.O()}]);
---
+++ _next/static/chunks/app/courses/web-development/layout-0971d92d1dcaa7bb.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[696],{90172:function(){}},function(n){n.O(0,[170,842,744],function(){return n(n.s=90172)}),_N_E=n.O()}]);
